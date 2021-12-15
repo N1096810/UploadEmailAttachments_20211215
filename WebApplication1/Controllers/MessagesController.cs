5cb286a3f833ab16b25ed3fa520a1abc621681b7
user <user@703-21>
2021-12-15 18:12:32 +0800

    Add project files.

---
+++ WebApplication1/Controllers/MessagesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models.ViewModels;
+
+namespace WebApplication1.Controllers
+{
+    public class MessagesController : Controller
+    {
+        // GET: Messages
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(EmailVM model)/*To receive objective "model" then to post it.*/
+        {
+            //If no file is uploaded, or if file name is null, or file content is 0, then show users errors.
+            if ((model.ToAttachFile == null) || (model.ToAttachFile.FileName == null) || (model.ToAttachFile.ContentLength == 0))
+            {
+                ModelState.AddModelError("ToAttachFile", "Attachments are mandatory.");/*Error 顯示在ToAttachFile旁邊*/
+                ModelState.AddModelError(String.Empty, "Attachments are mandatory.");/*Error 顯示在畫面上方*/
+                return View(model);
+            }
+            else
+            {
+                string path = Server.MapPath(@"~/UploadsAreStoredHere/");/*從根目錄尋找相對路徑*/
+                string newFileName = model.ToAttachFile.FileName;
+                string fullStoredPath = System.IO.Path.Combine(path + newFileName);
+
+                try 
+                {
+                    model.ToAttachFile.SaveAs(fullStoredPath);
+                    return RedirectToAction("Index");
+                } catch (Exception ex)
+                {
+                    ModelState.AddModelError("ToAttachFile", "Your attachment is not uploaded. Reason:" + ex.Message);
+                    return View(model);
+                }
+            }
+        }
+    }
+}
---
+++ WebApplication1/Models/ViewModels/EmailVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models.ViewModels
+{
+    public class EmailVM
+    {
+        [Required]
+        public string Subject { get; set; }
+        [Required]
+        [EmailAddress]
+        public string To { get; set; }
+        [Required]
+        public string Body { get; set; }
+        [Required]
+        public HttpPostedFileBase ToAttachFile { get; set; }
+    }
+}
